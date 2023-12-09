c574327f0f6eb62e59e1e57a717212ccf1b7bc06
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-09 09:23:54 +0300

    fix: edited method syntax for pycodestyle compliance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Boxes numbered 0 to n-1. Each box may contain keys to 
+Boxes numbered 0 to n-1. Each box may contain keys to
 the other boxes. Function determines whether all boxes
 can be opened starting from box 0
 """
@@ -19,7 +19,7 @@ def canUnlockAll(boxes):
         visited.add(currentKey)
         if len(locks) > 0:
             for newKey in locks:
-                if not(newKey in visited):
+                if not (newKey in visited):
                     if newKey < len(boxes):
                         unlocked.add(newKey)
     if len(visited) == n:
