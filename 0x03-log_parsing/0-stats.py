b26dc8464014707dec1f7903720e5efe95c7cc1c
Ngarotosto <carolvsreyesz@gmail.com>
2023-12-24 12:01:40 +0300

    fix: script edited to catch KeyboardInterrupt

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,44 +4,40 @@ Module reads script from stdin and computes metrics
 """
 
 import sys
-import signal
-
 
 lineNumber = 0
-currentStats = {}
-fileSize = 0
+status_codes = {}
+possible_status_code = [200, 301, 400, 401, 403, 404, 405, 500]
+file_size = 0
 
 
-def printstats(sig=0, frame=0):
+def printstats():
     """
     Display computed statistics
     """
-    print("File size: {}".format(fileSize))
-    for key in sorted(currentStats):
-        print("{}: {}".format(key, currentStats[key]))
+    print(f"File size: {file_size}")
+    for status, count in sorted(status_codes.items()):
+        print(f"{status}: {count}")
 
 
-signal.signal(signal.SIGINT, printstats)
-
-for line in sys.stdin:
-    try:
-        a = line.split(" ")
-        code = float(a[-2])
-        if (code in [200, 301, 400, 401, 403, 404, 405, 500]):
-            code = int(code)
-        else:
-            continue
-        cond1 = (len(a) == 9)
-        number = int(a[-1])
-        cond2 = (a[-3] == 'HTTP/1.1"')
-        cond3 = (a[-4] == '/projects/260')
-        cond4 = (a[-5] == '"GET')
-        if (cond1 and cond2 and cond3 and cond4):
-            currentStats[code] = currentStats.get(code, 0) + 1
-            fileSize += number
-            lineNumber += 1
-        if lineNumber % 10 == 0:
-            printstats()
-            continue
-    except Exception as e:
-        continue
+try:
+    for line in sys.stdin:
+        try:
+            line = line.split()
+            file_size = int(line[-1])
+            code = int(line[-2])
+            if code in possible_status_code and isinstance(code, int):
+                file_size += file_size
+                lineNumber += 1
+                if code in status_codes:
+                    status_codes[code] += 1
+                else:
+                    status_codes[code] = 1
+            if (lineNumber % 10) == 0:
+                printstats()
+        except ValueError:
+            pass
+except KeyboardInterrupt:
+    sys.exit(0)
+finally:
+    printstats()
